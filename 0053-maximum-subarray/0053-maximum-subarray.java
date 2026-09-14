d2724e913721641e79202db0e5aa1926533c5172
ankitguptaweb <ankitguptakhajuran@gmail.com>
2026-09-14 17:01:01 +0530

    Time: 1 ms (99.92%), Space: 77 MB (82.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum = nums[0];
@@ -10,3 +11,24 @@ class Solution {
          return sum;
     }
 }
+
+
+// class Solution {
+//     public int maxSubArray(int[] arr) {
+
+//         int n = arr.length;
+//         int ans = Integer.MIN_VALUE;
+//         int curMax = 0;
+
+//         for (int i = 0; i < n; i++) {
+
+//             int v1 = curMax + arr[i];
+//             int v2 = arr[i];
+
+//             curMax = Math.max(v1, v2);
+//             ans = Math.max(ans, curMax);
+//         }
+
+//         return ans;
+//     }
+// }
